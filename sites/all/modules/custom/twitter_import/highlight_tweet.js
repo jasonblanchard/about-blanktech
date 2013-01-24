18329634a87652a8861388d0f290c3cd63e044e0
Jason Blanchard <jas.blanchard@gmail.com>
2013-01-24 16:51:35 -0500

    Got the highlighting working the lazy way. Need to use CSS classes

--- sites/all/modules/custom/twitter_import/highlight_tweet.js
+++ sites/all/modules/custom/twitter_import/highlight_tweet.js
@@ -1,17 +1,22 @@
 (function($) {
    Drupal.behaviors.twitterImportHighlightTweet = {
        attach: function(context,settings) {
+
          $('.field-name-field-twitter-icon img.rollover').hover(function() {
            
            var id = $(this).attr('id');
 
-           var target = 'div#' + String(id);
+           var target = 'div#' + String(id) + ' p';
            
-           $(target).css('background','black');
-           console.log("mouse in");
+           $(target).css('color','#C27E34');
          }, 
          function() {
-           console.log("mouse out");
+
+           var id = $(this).attr('id');
+
+           var target = 'div#' + String(id) + ' p';
+
+           $(target).css('color','rgb(75, 74, 74)');
          });
        }
    }
