79245e7b56d43a6fd676ce129a655b2b8dbf81aa
jasonblanchard <jas.blanchard@gmail.com>
2013-03-22 17:08:06 -0400

    Fixed button toggle bug

--- sites/all/modules/custom/mobile_menu/mobile_menu.js
+++ sites/all/modules/custom/mobile_menu/mobile_menu.js
@@ -7,9 +7,9 @@
 
       $(menu).hide();
 
-      button = $('.mobile-menu-wrapper').prepend("<a href='#' class='mobile-button push-right'><span class='bar'></span><span class='bar'></span><span class='bar'></span></a>");
+      button = $('.mobile-menu-wrapper').prepend("<div><a href='#' class='mobile-button push-right'><span class='bar'></span><span class='bar'></span><span class='bar'></span></a><div>");
 
-      $(button).click(function() {
+      $('.mobile-button').click(function() {
         menu.slideToggle('fast');
 
         return false;
