eb51c0f1c42481d39102dfa2f991c3e8dbb735c0
vagrant <jas.blanchard@gmail.com>
2013-03-22 22:42:09 +0000

    Huge update

---
+++ sites/all/modules/custom/timelinejs/timeline_embed.js
@@ -0,0 +1,14 @@
+ // var timelineJsSource = Drupal.settings.timelinejs.embedSource
+
+  var timeline_config = {
+      width:              '100%',
+      height:             '600',
+      source:             'https://docs.google.com/spreadsheet/pub?key=0AlNyK0CWPkZjdE82U1dQNkRKRHdDaldmZUlnZUlBX0E&output=htmlt',
+      embed_id:           'timeline-embed',               //OPTIONAL USE A DIFFERENT DIV ID FOR EMBED
+      css:   'http://about.blanktech.net/sites/all/modules/custom/timelinejs/TimelineJS/compiled/css/timeline.css',
+      js: 'http://about.blanktech.net/sites/all/modules/custom/timelinejs/TimelineJS/compiled/js/timeline-min.js'
+  };
+
+jQuery(document).ready(function() {
+  jQuery('#timeline-embed').append(Drupal.settings.timelinejs.embedSource)
+});
